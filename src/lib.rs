d4912d5b08215fd705eea52299af7508ddd7b4ed
alarie <colin@colinheathcott.com>
2024-03-22 12:21:29 -0400

    Basic character ignore functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,50 @@
 
+pub fn ignore_parens(stream: &str) -> String {
+    if stream.contains("(") || stream.contains(")") {
+        let result = stream.replace("(", "").replace(")", "");
+        result
+    } else {
+        String::from(stream)
+    }
+}
 
+pub fn ignore_braces(stream: &str) -> String {
+    if stream.contains("{") || stream.contains("}") {
+        let result = stream.replace("(", "").replace(")", "");
+        result
+    } else {
+        String::from(stream)
+    }
+}
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn ignore_brackets(stream: &str) -> String {
+    if stream.contains("[") || stream.contains("]") {
+        let result = stream.replace("(", "").replace(")", "");
+        result
+    } else {
+        String::from(stream)
+    }
+}
+
+/// Use for statements wrapped in a character, like "string literals"
+/// Returns the provided &str with every instance of the char removed.
+pub fn ignore_wrapper(wrapper: char, stream: &str) -> String {
+    if stream.contains(wrapper) {
+        let result = stream.replace(wrapper, "");
+        result
+    } else {
+        String::from(stream)
+    }
 }
 
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-
-
-
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
+    #[test]
+    fn check_parens() {
+        let test_string = String::from("(hello)");
+        assert_eq!(ignore_parens(&test_string), "hello");
+    }
+}
